43d14df9d246adcfc609e5ecc95637cf67951390
InLineA <79007056+InLineA@users.noreply.github.com>
2025-02-01 18:34:27 +0300

    cleanup
    small app component cleanup in reakt-hooks

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,35 +1,9 @@
-import React, { useReducer } from 'react';
-import ComponentA from './components/useReducer/useReducer-useContext/ComponentA';
-import ComponentB from './components/useReducer/useReducer-useContext/ComponentB';
-import ComponentC from './components/useReducer/useReducer-useContext/ComponentC';
-
-const initialState = 0
-const reducer = (currentState, action) => {
-  switch(action){
-    case 'increment':
-      return currentState + 1
-    case 'decrement':
-      return currentState - 1
-    case 'reset':
-      return initialState
-    default:
-      return currentState
-  }
-}
-
-export const CountContext = React.createContext()
+import React from 'react';
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState)
-
   return (
     <div>
-      <CountContext.Provider value={{ countState: count, countDispatch: dispatch}}>
-        <h1>Count: {count}</h1>
-        <ComponentA />
-        <ComponentB />
-        <ComponentC />
-      </CountContext.Provider>
+      
     </div>
   );
 }
